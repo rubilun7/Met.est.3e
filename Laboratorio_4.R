38c86545bed51ff2a055cd31062ff0ad7d480b0d
rubi <lunabazalduav@gmail.com>
2025-08-28 09:28:29 -0600

    28 de agosto del 2025

--- Laboratorio_4.R
+++ Laboratorio_4.R
@@ -31,5 +31,57 @@ tapply(calidad$IE, calidad$Tratamiento, var)
 library("ggplot2")
 
 ggplot(calidad, aes(x = IE, color = Tratamiento))+geom_density()
+tapply(calidad$IE, calidad$Tratamiento, sd)
+
+# Separar los datos por tratamiento usando subset
+
+df_ctlr <- subset(calidad, Tratamiento == "Ctrl")
+df_fert <- subset(calidad, Tratamiento != "Ctrl")
+
+# qqnorm revisar normalidad
+par(mfrow =c(1, 2))
+qqnorm(df_ctlr$IE); qqline(df_ctlr$IE)
+qqnorm(df_fert$IE); qqline(df_fert$IE)
+par(mfrow = c(1, 1))
+
+
+# prueba de normalidad
+
+shapiro.test(df_ctlr$IE)
+shapiro.test(df_fert$IE)
+
+# Revisar homogeniedad de varianzas
+var.test(df_ctlr$IE, df_fert$IE)
+var.test(calidad$IE ~ calidad$Tratamiento)
+
+# Aplicar la prueba de t, varianzas iguales
+# dos colas = two.sided
+
+t.test(calidad$IE ~ calidad$Tratamiento, 
+       var.equal = T,
+       alternative = "two.sided")
+
+# Medir el efecto
+
+cohens_efecto <- function(x, y) {
+  n1 <- length(x); n2 <- length(y) 
+  s1 <-sd(x); s2 <- sd(y) 
+  sp <- sqrt(((n1 -1)*s1^2+(n2-1)*s2^2)/(n1+n2-2))
+  (mean(x) - mean(y)) / sp
+  }
+
+d_cal <- cohens_efecto(df_ctlr$IE, df_fert$IE)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
